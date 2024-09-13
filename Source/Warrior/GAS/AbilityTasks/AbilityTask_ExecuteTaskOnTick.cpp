7d39fe5c03fb552a0b1d1f220aa8a6289bd2d6f0
Wliered <wliered@proton.me>
2024-09-12 22:17:17 -0500

    164 -- Custom AbilityTask
    This one was confusing. See my Trello notes for details.
    
    164 -- Additional
    
    forgot to save and forgot how to properlay handle the Git side :(

---
+++ Source/Warrior/GAS/AbilityTasks/AbilityTask_ExecuteTaskOnTick.cpp
@@ -0,0 +1,29 @@
+﻿// Wliered did a thing
+
+
+#include "AbilityTask_ExecuteTaskOnTick.h"
+
+UAbilityTask_ExecuteTaskOnTick::UAbilityTask_ExecuteTaskOnTick()
+{
+	bTickingTask = true;
+}
+
+UAbilityTask_ExecuteTaskOnTick* UAbilityTask_ExecuteTaskOnTick::ExecuteTaskOnTick(UGameplayAbility* OwningAbility)
+{
+	UAbilityTask_ExecuteTaskOnTick* Node = NewAbilityTask<UAbilityTask_ExecuteTaskOnTick>(OwningAbility);
+	return Node;
+}
+
+void UAbilityTask_ExecuteTaskOnTick::TickTask(float DeltaTime)
+{
+	Super::TickTask(DeltaTime);
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		OnAbilityTaskTick.Broadcast(DeltaTime);
+	}
+	else
+	{
+		EndTask();
+	}
+}
