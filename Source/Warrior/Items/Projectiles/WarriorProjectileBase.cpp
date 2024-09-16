1ca847966d746cc9f33d3e3bd97413f21db8005d
Wliered <wliered@proton.me>
2024-09-16 10:33:05 -0500

    186-7 -- Projectile Callbacks & OnHit Implementation
    On the collision box component, setup callback functions for OnComponentHit and OnComponentBeginOverlap
    
    Implemented the previously created callback for OnProjectileHit
    
    calls a BP Implementable event for FX spawning
    
    casts other Actor to HitPawn
    
    Checks if HitPawn is valid, or is HitPawn is hostile (warrior library function)
    
    destroys projectile if it hits something other than player
    
    checks for blocking using warrior function library
    
    sends gameplay event data to player if block is successful (previously implemented perfect block timing should still be effective)
    
    if block is not valid, applies damage (tbi – to be implemented)
    
    finally destroys the actor if it hasn’t been already
    
    Implemented the BP Event within the BP_Projectile_Base to spawn sound and FX at the impact point
    OnProjectileHit – 5 params – .AddUniqueDynamic
    
    OnProjectileBeginOverlap – 6 params – .AddUniqueDynamic
    
    NOTE: all our current collision responses are set to Block, so the BeginOverlap callback will never be called
    
    We can adjust the collision responses in BeginPlay() by checking the enum we created

--- Source/Warrior/Items/Projectiles/WarriorProjectileBase.cpp
+++ Source/Warrior/Items/Projectiles/WarriorProjectileBase.cpp
@@ -3,9 +3,13 @@
 
 #include "WarriorProjectileBase.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
 #include "NiagaraComponent.h"
 #include "Components/BoxComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Warrior/WarriorDebugHelper.h"
+#include "Warrior/WarriorFunctionLibrary.h"
+#include "Warrior/WarriorGameplaytags.h"
 
 
 AWarriorProjectileBase::AWarriorProjectileBase()
@@ -18,6 +22,8 @@ AWarriorProjectileBase::AWarriorProjectileBase()
 	ProjectileCollisionBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Block);
 	ProjectileCollisionBox->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Block);
 	ProjectileCollisionBox->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
+	ProjectileCollisionBox->OnComponentHit.AddUniqueDynamic(this, &ThisClass::OnProjectileHit);
+	ProjectileCollisionBox->OnComponentBeginOverlap.AddUniqueDynamic(this, &ThisClass::OnProjectileBeginOverlap);
 
 	ProjectileNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Niagara Component"));
 	ProjectileNiagaraComponent->SetupAttachment(GetRootComponent());
@@ -34,7 +40,53 @@ AWarriorProjectileBase::AWarriorProjectileBase()
 void AWarriorProjectileBase::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (ProjectileDamagePolicy == EProjectileDamagePolicy::OnBeginOverlap)
+	{
+		ProjectileCollisionBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	}
+}
+
+void AWarriorProjectileBase::OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	BP_OnSpawnProjectileGitFX(Hit.ImpactPoint);
 	
+	APawn* HitPawn = Cast<APawn>(OtherActor);
+	
+	if (!HitPawn || !UWarriorFunctionLibrary::IsTargetPawnHostile(GetInstigator(), HitPawn))
+	{
+		Destroy();
+		return;
+	}
+
+	bool bIsValidBlock = false;
+	const bool bIsPlayerBlocking = UWarriorFunctionLibrary::NativeDoesActorHaveTag(HitPawn, WarriorGameplayTags::Player_Status_Blocking);
+
+	if (bIsPlayerBlocking)
+	{
+		bIsValidBlock = UWarriorFunctionLibrary::IsValidBlock(this, HitPawn);
+	}
+
+	FGameplayEventData Data;
+	Data.Instigator = this;
+	Data.Target = HitPawn;
+	
+	if (bIsValidBlock)
+	{
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(HitPawn, WarriorGameplayTags::Player_Event_SuccessfulBlock, Data);
+	}
+	else
+	{
+		//Apply Projectile Damage
+	}
+
+	Destroy();
+}
+
+void AWarriorProjectileBase::OnProjectileBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
 }
 
 
--- Source/Warrior/Items/Projectiles/WarriorProjectileBase.h
+++ Source/Warrior/Items/Projectiles/WarriorProjectileBase.h
@@ -28,6 +28,13 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit );
+	UFUNCTION()
+	virtual void OnProjectileBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "OnSpawnHitFX"))
+	void BP_OnSpawnProjectileGitFX(const FVector& HitLocation);
+	
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Projectile")
 	UBoxComponent* ProjectileCollisionBox;
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Projectile")
