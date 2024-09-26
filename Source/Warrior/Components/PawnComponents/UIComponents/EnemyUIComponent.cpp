b6102fa106d16d1c906cecf1b481f2043e4aea26
Wliered <wliered@proton.me>
2024-09-26 09:19:36 -0500

    203 Removing the Boss Health Bar
    in our EnemyUIComponent, we added a private variable of type TArray<UWarriorWidgetBase> to hold all the widgets we allocate to the Boss
    
    Then we created two Blueprint Callable functions. 1 for adding new widgets to the array, and one for removing all widgets in the array from their parent
    
    Then we register the health bar widget using the 1st new function by calling it from within the GA_DrawBossBar_Base, and remove it by calling the 2nd function from within the BP_Enemy_Base in the handle death function

--- Source/Warrior/Components/PawnComponents/UIComponents/EnemyUIComponent.cpp
+++ Source/Warrior/Components/PawnComponents/UIComponents/EnemyUIComponent.cpp
@@ -3,6 +3,26 @@
 
 #include "EnemyUIComponent.h"
 
+#include "Warrior/Widgets/WarriorWidgeBase.h"
 
 
+void UEnemyUIComponent::RegisterEnemyDrawnWidget(UWarriorWidgeBase* InWidgetToRegister)
+{
+	EnemyDrawnWidgets.Add(InWidgetToRegister);
+}
 
+void UEnemyUIComponent::RemoveEnemyDrawnWidgetsIfAny()
+{
+	if (EnemyDrawnWidgets.IsEmpty())
+	{
+		return;
+	}
+
+	for (UWarriorWidgeBase* DrawnWidget : EnemyDrawnWidgets)
+	{
+		if (DrawnWidget)
+		{
+			DrawnWidget->RemoveFromParent();
+		}
+	}
+}
--- Source/Warrior/Components/PawnComponents/UIComponents/EnemyUIComponent.h
+++ Source/Warrior/Components/PawnComponents/UIComponents/EnemyUIComponent.h
@@ -7,9 +7,20 @@
 #include "EnemyUIComponent.generated.h"
 
 
+class UWarriorWidgeBase;
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class WARRIOR_API UEnemyUIComponent : public UPawnUIComponent
 {
 	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	void RegisterEnemyDrawnWidget(UWarriorWidgeBase* InWidgetToRegister);
+
+	UFUNCTION(BlueprintCallable)
+	void RemoveEnemyDrawnWidgetsIfAny();
+
+private:
+	TArray<UWarriorWidgeBase*> EnemyDrawnWidgets;
 	
 };
